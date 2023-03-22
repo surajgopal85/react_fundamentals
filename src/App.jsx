e690987bdd428d8f9361b23bc46524cac04d6e14
Suraj Gopal <surajcancode@gmail.com>
2023-03-22 19:18:07 -0400

    new react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>Gotta start remembering stuff</p>
+      <p>This is part of a component.
+      Remember parens for multi-line return in func component.</p>
+    </div>
+  );
+}
+
+export default App;
