9c61ce6678560bf2749631e5bc153db7933e63ed
Suraj Gopal <surajcancode@gmail.com>
2023-03-23 10:16:31 -0400

    used props to pass expenses array to ExpenseItem component, cleaned up date with Date methods

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import '../styles/ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long'});
+  const day = props.date.toLocaleString('en-US', { day: '2-digit'});
+  const year = props.date.getFullYear();
+
   return (
-    <div>ExpenseItem</div>
-  )
-}
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
