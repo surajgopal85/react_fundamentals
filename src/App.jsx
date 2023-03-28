eadaeb5c1af0de35148d8ecc2050d5d5fa6b2f45
Suraj Gopal <surajcancode@gmail.com>
2023-03-28 12:02:31 -0400

    refactored Expenses component to map expenses arr as items prop into ExpenseItem component config

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,12 @@ const App = () => {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: 'e5',
+      title: 'Organizers',
+      amount: 100,
+      date: new Date(2023, 3, 28),
+    }
   ];
   return (
     <div>
