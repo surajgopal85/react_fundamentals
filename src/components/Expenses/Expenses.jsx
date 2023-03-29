ff8e952ce6758f415627bf2bbae00c6a201e99ec
Suraj Gopal <surajcancode@gmail.com>
2023-03-29 09:08:32 -0400

    changed ExpenseItem to stateless component handled by parent Expenses

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "../../styles/Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const onChangeFilterYearHandler = (selectedYear) => {
+    console.log(`in expenses: ${selectedYear}`)
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((item) => {
-        return(
-          <ExpenseItem
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilterYear={onChangeFilterYearHandler}
+        />
+        {props.items.map((item) => {
+          return (
+            <ExpenseItem
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
