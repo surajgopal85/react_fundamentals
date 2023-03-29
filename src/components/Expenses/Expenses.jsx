7da2494a5e0bf8bcad3201546253b15403192a6e
Suraj Gopal <surajcancode@gmail.com>
2023-03-29 09:16:19 -0400

    tweaked array map for converting arr of expenses to ExpenseItem components

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -19,15 +19,13 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilterYear={onChangeFilterYearHandler}
         />
-        {props.items.map((item) => {
-          return (
+        {props.items.map((item) => (
             <ExpenseItem
               title={item.title}
               amount={item.amount}
               date={item.date}
             />
-          );
-        })}
+      ))}
       </Card>
     </div>
   );
